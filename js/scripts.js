92eed857e76f79147c9d1e0c78de6533efb94e8f
jihwanlim <jihwan6933@gmail.com>
2023-11-14 05:58:48 +0900

    gallery

--- js/scripts.js
+++ js/scripts.js
@@ -37,4 +37,16 @@ $('#collapseExample').collapse({
     toggle: true
 })
 
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+        // If the element is visible
+        if (entry.isIntersecting) {
+            // Add the animation class
+            entry.target.classList.add('animate');
+        }
+    });
+});
+
+observer.observe(document.querySelector('.square'));
 
